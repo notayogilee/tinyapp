89c16bc5bd5e1aa528fe85429f0fbbc6aefa53d8
Lee <notayogilee@gmail.com>
2020-02-17 19:15:47 +0000

    Some HTML text added

--- express_server.js
+++ express_server.js
@@ -11,9 +11,14 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
