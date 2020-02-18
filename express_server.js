3d99246fc8ad8478faf0ddfea3a6915a379ce4d4
Lee <notayogilee@gmail.com>
2020-02-18 01:32:39 +0000

    first part..done

--- express_server.js
+++ express_server.js
@@ -39,12 +39,20 @@ app.get('/urls', (req, res) => {
 });
 
 app.post('/urls', (req, res) => {
-  console.log(req.body);
-  res.send('ok');
+  let shortURL;
+  shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  console.log(urlDatabase);
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
+});
+
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
