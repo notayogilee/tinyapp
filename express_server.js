65c470efcc24d5b5c16a5f29711c8366435b77b0
Lee Castelani <notayogilee@gmail.com>
2020-02-19 08:21:01 -0500

    login or logged in form alternative

--- express_server.js
+++ express_server.js
@@ -36,7 +36,7 @@ app.listen(PORT, () => {
 });
 
 app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render('urls_index', templateVars);
 });
 
@@ -53,11 +53,12 @@ app.post('/urls', (req, res) => {
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  let templateVars = { username: req.cookies["username"] }
+  res.render('urls_new', templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   res.render('urls_show', templateVars);
 });
 
@@ -74,10 +75,10 @@ app.get('/urls', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
+
   res.cookie('username', req.body.username);
   res.redirect('/urls');
 });
-
 
 
 app.get('/u/:shortURL', (req, res) => {
